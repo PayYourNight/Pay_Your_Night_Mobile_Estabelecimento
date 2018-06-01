3222e73be8871f4239c60201e8e8b68a7976cc27
Romilda-PC\Eduardo <eduardo.x.aguiar@gmail.com>
2018-05-31 21:08:15 -0300

    socket.io

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { Socket } from 'ng-socket-io';
 
 @Component({
   selector: 'page-home',
@@ -7,8 +8,20 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+  constructor(public navCtrl: NavController, private socket: Socket) {
 
+  }
+
+  sendMessageCheckinMOCK(){
+    this.socket.emit("checkin", "checkin realizado");
+  }
+
+  sendMessageCheckoutMOCK(){
+    this.socket.emit("checkout", "checkout realizado");
+  }
+  
+  sendMessageConsumoMOCK(){
+    this.socket.emit("message", "consumo realizado");
   }
 
 }
