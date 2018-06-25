8144f25485db8585d8f732412002551bd1f789ac
eduardo <eduardo.x.aguiar@gmail.com>
2018-06-25 01:02:58 -0300

    pagamento

--- src/pages/checkin/qrcode-checkin.ts
+++ src/pages/checkin/qrcode-checkin.ts
@@ -16,86 +16,13 @@ export class QrcodeCheckinPage {
   ) { 
     
     let user = localStorage.getItem('user');    
-
-    // socket.emit('checkinConnect', {
-    //   user: this.usuarioId,
-    //   role: 'caixa',
-    //   details: {
-    //     estabelecimento: {
-    //       id : this.estabelecimentoId,
-    //       nome: this.estabelecimentoNome
-    //     }
-    //   }
-    // });
   }
-
-//   presentProfileModal() {
-//     let profileModal = this.modalCtrl.create(ModalConfirmacaoPage);
-//     profileModal.present();
-//   }
   
   scanCode() {
     this.barcodeScanner.scan().then(barcodeData => {
-      //this.scannedCode = barcodeData.text;
-      
-      //TODO
-      //this.criarCheckinBD()
-
-      //this.emitirMensagemServidor();
       
     }, (err) => {
         console.log('Error: ', err);
     });
   }
-
-//   simularCheckin(){          
-//       this.criarCheckinBD()
-
-//       this.emitirMensagemServidor();
-//   }
-
-//   criarCheckinBD(){
-//     this.service.criarCheckin(this.usuarioId, this.estabelecimentoId, this.transactionId)
-//     .subscribe(
-//       (data) => {
-//         if (data){
-
-//         }
-//       });   
-//   }
-
-//   emitirMensagemServidor(){
-//     this.socket.emit("checkin", { 
-//       "estabelecimentoId": this.estabelecimentoId,
-//       "transactionId": this.transactionId 
-//     });
-//   }
-
-  // presentToast() {
-  //   let toast = this.toastCtrl.create({
-  //     message: 'Check-in realizado com sucesso.',
-  //     position: 'top',
-  //     duration: 3000
-  //   });
-  //   toast.present();
-  // }
-
-//   dismiss() {
-//     this.viewCtrl.dismiss();
-//   }
 }
-
-/*********************************/
-
-// @Component({
-//   selector: 'page-modal-confirmacao',
-//   templateUrl: 'modal-confirmacao.html'
-// })
-// export class ModalConfirmacaoPage {
-//   constructor(public viewCtrl: ViewController) {
-//   }
-
-//   dismiss() {
-//     this.viewCtrl.dismiss();
-//   }
-// }
